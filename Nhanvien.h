696906e5cae37e4b6a374678c373b20e9ec0fe2a
namvietanh1902 <85499555+namvietanh1902@users.noreply.github.com>
2021-10-06 08:16:35 +0700

    Khong co header file a

---
+++ Nhanvien.h
@@ -0,0 +1,26 @@
+#ifndef NHANVIEN
+#define NHANVIEN
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+#include<conio.h>
+using namespace std;
+class Nhanvien{
+    private:
+        int stt;
+        string name;
+        int age;
+        string address;
+        string sdt;
+    public:
+        Nhanvien(){}
+        friend std::ostream& operator <<(std::ostream & ,const Nhanvien &);
+        friend std::istream& operator >>(std::istream &,Nhanvien &);
+        friend void getInfo(Nhanvien*,int n);
+        void setName(string name);
+        string getName();
+
+};
+void stringCorrect(string &);
+#endif
