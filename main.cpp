5b7b71a46cc7063d7d7eca1fad4b165fdc16a87a
vanthinn <thinh209202@gmail.com>
2021-10-17 10:51:22 +0700

    loi cc

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ int main(){
     int n=1000;
     Item *Menu=new Item[n];
     Nhanvien *NV=new Nhanvien[n];
- 
+
     display(Menu);
-  
-    Delete(Menu);
+    Delete
+    (Menu);
     display(Menu);
     delete [] NV;
     delete [] Menu;
