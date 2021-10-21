a10d91600f6713cc30ed15bfdad9e07fec8d8077
namvietanh1902 <85499555+namvietanh1902@users.noreply.github.com>
2021-10-21 18:28:11 +0700

    fix bug menu

--- Menu.cpp
+++ Menu.cpp
@@ -13,17 +13,31 @@ void Manager(){
     Man:
     cout<<"Enter your username: ";cin>>_Username;
     cout<<"Enter your password: ";
-    while ((c=_getch()) !=13)
+    int i=0;
+    _Pass="";
+    do
 {
-    _Pass.push_back(c);
-    _putch('*');
-}
+    c = _getch();
+
+    if (c == 13 || c == ' ' )
+    {
+        break;
+    }
+    if((c==8 || c==127) && !_Pass.empty())
+    {
+        cout<<"\b \b";
+        _Pass.erase(_Pass.size()-1);
+    }
+   else{ _Pass.push_back(c);
+    _putch('*');}
+
+}while(c!=13 || c!=' ');
     if (_Username==username&&pass==_Pass) cout<<"\nDang nhap thanh cong\n";
     else {cout<<"\nSai mat khau.\n1.Dang nhap lai\n2.Ket thuc\nNhap lua chon: ";
     char d;cin>>d;
     switch(d){
         case '1':{
-            Manager();
+            goto Man;
             break;
         }
         case '2':{
@@ -40,25 +54,64 @@ void resetPass(){
     in>>username>>pass;
     cout<<"Vui long nhap mat khau cu: ";
     char c;
-    while ((c=_getch()) !=13)
+    _Pass="";
+   do
 {
-    _Pass.push_back(c);
-    _putch('*');
-}
+    c = _getch();
+
+    if (c == 13 || c == ' ' )
+    {
+        break;
+    }
+    if((c==8 || c==127) && !_Pass.empty())
+    {
+        cout<<"\b \b";
+        _Pass.erase(_Pass.size()-1);
+    }
+   else{ _Pass.push_back(c);
+    _putch('*');}
+
+}while(c!=13 || c!=' ');
     if (_Pass==pass){
         datlai:
+        newpass="";
+        data="";
         cout<<"\nNhap mat khau moi: ";
-          while ((c=_getch()) !=13)
-        {
-        newpass.push_back(c);
-        _putch('*');
-        }
+       do
+    {
+        c =   _getch();
+
+    if (c == 13 || c == ' ' )
+    {
+            break;
+    }
+    if((c==8 || c==127) && !newpass.empty())
+    {
+            cout<<"\b \b";
+            newpass.erase(newpass.size()-1);
+    }
+        else{ newpass.push_back(c);
+        _putch('*');}
+
+    }while(c!=13 || c!=' ');
         cout<<"\nNhap lai mat khau moi: ";
-         while ((c=_getch()) !=13)
-        {
-        data.push_back(c);
-        _putch('*');
-        }
+        do
+{
+    c = _getch();
+
+    if (c == 13 || c == ' ' )
+    {
+        break;
+    }
+    if((c==8 || c==127) && !data.empty())
+    {
+        cout<<"\b \b";
+        data.erase(data.size()-1);
+    }
+   else{ data.push_back(c);
+    _putch('*');}
+
+}while(c!=13 || c!=' ');
         if (newpass==data){
             cout<<"\nDa doi mat khau thanh cong.\n";
             ofstream out;
@@ -67,7 +120,7 @@ void resetPass(){
             out.close();
         }
         else{
-            cout<<"Mat khau khong khop, dat lai khong?\n1.Co\n2.Khong\nNhap lua chon: ";
+            cout<<"\nMat khau khong khop, dat lai khong?\n1.Co\n2.Khong\nNhap lua chon: ";
             char d; cin>>d;
             switch(d){
                 case '1':{
@@ -92,6 +145,7 @@ void resetPass(){
 
 }
 int main(){
+   
     Manager();
     resetPass();
 }
